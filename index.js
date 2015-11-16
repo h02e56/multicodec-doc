75757f94527c8ef2ed726368e157e3dd34056d84
h02e56 <josep@h02e56.com>
2015-11-16 01:50:37 +0100

    show response

--- index.js
+++ index.js
@@ -1,61 +1,58 @@
-var streamPair = require('stream-pair');
-var Interactive = require('multistream-select').Interactive;
+var streamPair = require('stream-pair')
+var Interactive = require('multistream-select').Interactive
 var Select = require('multistream-select').Select
 
-var pair = streamPair.create();
-var msi = new Interactive();
-var mss = new Select();
+var pair = streamPair.create()
+var msi = new Interactive()
+var mss = new Select()
 
 var events = require('./lib/events')
 
-mss.handle(pair.other);
+mss.handle(pair.other)
 
-mss.addHandler('/xxx/', function(ds) {
-  ds.on('data', function(chunk) {
-    console.log('que si');
-  });
-  ds.on('end', function() {
-    ds.end();
-  });
-});
+mss.addHandler('/xxx/', function (ds) {
+  ds.on('data', function (chunk) {
+    showResponse('get ur data: ' + chunk.toString())
+  })
+  ds.on('end', function () {
+    ds.end()
+  })
+})
 
-mss.addHandler('/wtf/', function(ds) {
-  ds.on('data', function(chunk) {
-    console.log(chunk.toString());
-    console.log('w');
-  });
-  ds.on('end', function() {
-    ds.end();
-  });
-});
+mss.addHandler('/wtf/', function (ds) {
+  ds.on('data', function (chunk) {
+    console.log(chunk.toString())
+    console.log('w')
+  })
+  ds.on('end', function () {
+    ds.end()
+  })
+})
 
-
-msi.handle(pair, function() {
-  msi.ls(function(err, result) {
+msi.handle(pair, function () {
+  msi.ls(function (err, result) {
     if (err) {
       return console.log(err)
     }
-    console.log('results: ', result);
-  });
+    console.log('protocols supported: ', result)
+  })
+})
 
-  msi.select('/wtf/', function(err, ds) {
-    if (err) {
-      return console.log(err)
-    }
-
-    ds.write('wollll');
-    ds.end();
-  });
-});
-
-var client = events();
-
-client.init()
+var client = events()
 
 client.on('fail', function () {
   console.log('fail')
 })
 
 client.on('message', function (message) {
-  console.log(message)
+  msi.select(message.protocol, function (err, ds) {
+    if (err) return showResponse(err)
+
+    ds.write(message.data)
+    ds.end()
+  })
 })
+
+function showResponse(message) {
+  document.getElementById('response').textContent = message
+}
